9be284ce761de4ab64392ac27bcde93ca293b86a
Yajur21 <61120383+Yajur21@users.noreply.github.com>
2021-02-16 09:52:49 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Yajur Tomar on 2/16/21.
+//  Copyright © 2021 yajur.tomar@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
